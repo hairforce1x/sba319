3fb616eacd7711b0e60735b32e973bb1aca98431
hairforce1x <93629403+hairforce1x@users.noreply.github.com>
2025-02-04 20:31:41 -0700

    more routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,18 +4,33 @@ const router = express.Router();
 const Golfer = require('../models/Golfer');
 const Course = require('../models/Course');
 const Score = require('../models/Score');
-const exampleData = require('../data/data.json');
+const data = require('../data/data.json');
 
 router.use(express.json());
 
 // Get all golfers
 router.get('/golfers', async (req, res) => {
     try {
-        const golfers = await Golfer.find();
-        res.json(golfers);
+       res.json(data.golfers)
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
+
+router.get('/courses', async(req, res) => {
+    try{
+        res.json(data.courses)
+    } catch (err) {
+        res.status (500).json ({ error: err.message })
+    }
+})
+
+router.get('/scores', async(req, res) => {
+    try{
+        res.json(data.scores)
+    } catch (err) {
+        res.status (500).json ({ error: err.message })
+    }
+})
 
 module.exports = router;
