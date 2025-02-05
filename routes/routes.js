149451b3aa61caf506ec0d7b39e43c3c7cf7c41b
hairforce1x <93629403+hairforce1x@users.noreply.github.com>
2025-02-04 20:51:34 -0700

    added out and delete routes

--- routes/routes.js
+++ routes/routes.js
@@ -8,28 +8,71 @@ const data = require('../data/data.json');
 
 router.use(express.json());
 
-// Get all golfers
 router.get('/golfers', async (req, res) => {
     try {
-       res.json(data.golfers)
+        res.json(data.golfers)
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
 
-router.get('/courses', async(req, res) => {
-    try{
+router.post('/golfers', async (req, res) => {
+    try {
+        const { name } = req.body;
+        const newGolfer = new Golfer({
+            name
+        });
+        await newGolfer.save();
+        res.status(201).json(newGolfer);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.put('/golfers/:id', async (req, res) => {
+    try {
+        const golferId = req.params.id;
+        const { name } = req.body;
+        const updatedGolfer = await Golfer.findByIdAndUpdate(
+            golferId,
+            { name },
+            { new: true }
+        );
+        if (!updatedGolfer) {
+            return res.status(404).json({ error: "Golfer not found" });
+        }
+        res.json(updatedGolfer);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.delete('/golfers/:id', async (req, res) => {
+    try {
+        const golferId = req.params.id;
+        const deletedGolfer = await Golfer.findByIdAndDelete(golferId);
+        if (!deletedGolfer) {
+            return res.status(404).json({ error: "Golfer not found" });
+        }
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+router.get('/courses', async (req, res) => {
+    try {
         res.json(data.courses)
     } catch (err) {
-        res.status (500).json ({ error: err.message })
+        res.status(500).json({ error: err.message })
     }
 })
 
-router.get('/scores', async(req, res) => {
-    try{
+router.get('/scores', async (req, res) => {
+    try {
         res.json(data.scores)
     } catch (err) {
-        res.status (500).json ({ error: err.message })
+        res.status(500).json({ error: err.message })
     }
 })
 
